62f18fc0c76b737723b82265e66302b3bc49f949
Renato Souza <renatorock3@hotmail.com>
2020-05-30 01:25:01 -0300

    Ajuste Layout

--- src/screens/List/index.js
+++ src/screens/List/index.js
@@ -1,32 +1,52 @@
-import React, {Component} from 'react';
+import React, {useState, Component} from 'react';
 import {View, Text, TouchableOpacity, FlatList, Image} from 'react-native';
 
 import styles from './styles';
 
+import api from '../../service/api';
+
 export default class List extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
+
+  state = {
+    pokemon: []
+  }
+
+  async componentDidMount() {
+
+    const { data } = await api.get('pokemon');
+
+    this.setState({ pokemon: [...data.results] });
+    
+
   }
 
   render() {
     return (
       <View style={styles.container}>
-        <View style={styles.incident}>
-          <View style={{ flexDirection: 'row', justifyContent: 'space-between'}}>
-            <Text style={styles.labelValue}>Bulbasaur</Text>
-            <Image 
-              source={require('../../assets/bulbasaur.png')} 
-              style={{ width: 70, height: 70 }}
-            />
-          </View>
-          <TouchableOpacity 
-              style={styles.detailsButton} 
-              onPress={() => navigateToDetail(incident)}
-          >
-            <Text style={styles.detailsButtonText}>Ver mais detalhes</Text>
-          </TouchableOpacity>
-        </View>                        
+        <FlatList 
+          data={this.state.pokemon}
+          keyExtractor={()=>{}}
+          showsVerticalScrollIndicator={false}
+          // onEndReached={loadIncidents}
+          onEndReachedThreshold={0.2}
+          renderItem={({ item: pokemon }) =>(
+            <View style={styles.incident}>
+              <View style={{ flexDirection: 'row', justifyContent: 'space-between'}}>
+                <Text style={styles.labelValue}>{pokemon.name}</Text>
+                <Image 
+                  source={require('../../assets/bulbasaur.png')} 
+                  style={{ width: 70, height: 70 }}
+                />
+              </View>
+              <TouchableOpacity 
+                  style={styles.detailsButton} 
+                  onPress={() => navigateToDetail(incident)}
+              >
+                <Text style={styles.detailsButtonText}>Ver mais detalhes</Text>
+              </TouchableOpacity>
+          </View>                         
+          )}
+        />
       </View>
     );
   }
